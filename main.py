56aa126c04ff206f6bc06fc9f4251c585bb5efe6
Joel Mendez <joelmz@outlook.com>
2026-03-01 08:55:00 +0100

    rename tasks.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template, request, jsonify
-import tasks
+import task
 
 app = Flask(__name__)
 
 @app.route('/') 
 def home():
-    tasks.init_db()
+    task.init_db()
     return render_template('index.html')
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify(tasks.get_tasks())
+    return jsonify(task.get_tasks())
 
 @app.route('/create-task', methods=['POST'])
 def create_task():
     data = request.get_json()
     name = data.get("message", "")
-    task_id = tasks.create_task(name)
+    task_id = task.create_task(name)
     return jsonify({"id": task_id, "name": name})
 
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
     data = request.get_json()
-    tasks.delete_task(data.get("id"))
+    task.delete_task(data.get("id"))
     return jsonify({"ok": True})
 
 if __name__ == '__main__':
